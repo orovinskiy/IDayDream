880d1774999c00326f93c7ee1a8b611b5c9b120c
olegr <orovinskiy@mail.greenriver.edu>
2019-10-13 17:15:59 -0700

    added styling (not compatible with JS!)

---
+++ Volunteer.js
@@ -0,0 +1,13 @@
+/*// Puts the buttons into a array
+let buttonList = document.getElementsByTagName("button");
+
+// accesses all the buttons for the availability
+
+for(let i = 0; i < buttonList.length; i++){
+    buttonList[i].addEventListener("click",function(){buttonType(i)})
+}
+
+function buttonType(week){
+    this.className= "hidden";
+    console.log("i went through");
+}*/
