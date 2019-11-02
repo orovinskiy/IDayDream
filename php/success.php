1cbca2cfb8927512e1fd02ed071ed59b8f4358e2
olegr <orovinskiy@mail.greenriver.edu>
2019-11-02 10:46:12 -0700

    Created thank you page for welcome form. Updated welcomes form Ethnicity values to be more user friendly commented out error reporting

--- php/success.php
+++ php/success.php
@@ -1,7 +1,7 @@
 <?php
 //Error Reporting
-ini_set("display_errors",1);
-error_reporting(E_ALL);
+/*ini_set("display_errors",1);
+error_reporting(E_ALL);*/
 
 //Fields
 $firstName = $_POST["firstName"];
