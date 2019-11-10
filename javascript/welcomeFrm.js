322a3e2945c8abc2d6b009304512d21a637c1569
olegr <orovinskiy@mail.greenriver.edu>
2019-11-09 16:06:50 -0800

    Changed match() to test()

--- javascript/welcomeFrm.js
+++ javascript/welcomeFrm.js
@@ -88,7 +88,7 @@ function validateFormat(input, regexp) {
     let inputTxt = input.value.trim();
 
     // only display msg for valid format if characters are typed in
-    if (inputTxt !== "" && !regexp.match(inputTxt)) {
+    if (inputTxt !== "" && !regexp.test(inputTxt)) {
 
         // error message id is the id of the input it belongs to + "ErrFormat"
         let errMsg = document.getElementById(input.id + "ErrFormat");
